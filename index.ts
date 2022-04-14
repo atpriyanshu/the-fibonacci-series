de9f6d21c2434aa2e0d2fed5573daed4bde78276
atpriyanshu <priyanshu2007pk@gmail.com>
2022-04-14 18:29:42 +0530

    Add: Add TypeScript

---
+++ index.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log(fibonacci(01));  // returns 0
+console.log(fibonacci(02));  // returns 1
+console.log(fibonacci(03));  // returns 1
+console.log(fibonacci(15));  // returns 377
+console.log(fibonacci(20));  // returns 4181
+
+function fibonacci(x) {
+  if (x <= 2) {
+    return x - 1;
+  }
+
+  return fibonacci(x - 2) + fibonacci(x - 1);
+}
